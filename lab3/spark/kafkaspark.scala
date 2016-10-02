341e43c9e18133b94fdd3a7e237f65c20083a802
anestos <n_anestos@yahoo.gr>
2016-10-02 15:06:30 +0200

    lab3

--- lab3/spark/kafkaspark.scala
+++ lab3/spark/kafkaspark.scala
@@ -17,24 +17,32 @@ object KafkaWordCount {
 	"group.id" -> "kafka-spark-streaming",
 	"zookeeper.connection.timeout.ms" -> "1000")
 
-    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster(<FILL IN>)
-    val ssc = new StreamingContext(<FILL IN>)
+    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster("local[2]")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
     ssc.checkpoint("checkpoint")
 
     // if you want to try the receiver-less approach, comment the below line and uncomment the next one
-    val messages = KafkaUtils.createStream[String, String, DefaultDecoder, StringDecoder](<FILL IN>)
-    //val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](<FILL IN>)
-
-    val values = messages.map(<FILL IN>)
-    val pairs = values.<FILL IN>
-
+    // val messages = KafkaUtils.createStream[String, String, DefaultDecoder, StringDecoder](ssc, kafkaConf, Map("avg" -> 1), StorageLevel.MEMORY_ONLY)
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaConf, Set("avg"))
+   
+    val values = messages.map( s1 => s1._2)
+    val pairs = values.map(
+        
+            record =>
+            (record.toString.split(",")(0), record.toString.split(",")(1).toDouble)
+        )
+    
 
     def mappingFunc(key: String, value: Option[Double], state: State[Double]): Option[(String, Double)] = {
-	<FILL IN>
+
+        val sum = (value.getOrElse(0.0) + state.getOption.getOrElse(0.0)) /2
+        val output = (key, sum)
+        state.update(sum)
+        Option(output)
+
     }
 
-
-    val stateDstream = pairs.mapWithState(<FILL IN>)
+    val stateDstream = pairs.mapWithState(StateSpec.function(mappingFunc _))
 
     stateDstream.print()
     ssc.start()
